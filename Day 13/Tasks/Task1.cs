f89f307e41d0601d7e553cca3200bd49d9de10e5
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-16 13:56:39 +0530

    Created Tasks

---
+++ Day 13/Tasks/Task1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+class Task1
+{
+    static void Main()
+    {
+        Thread ch1Th = new Thread(PerformAction);
+        Thread ch2Th = new Thread(PerformAction);
+        Thread ch3Th = new Thread(PerformAction);
+        ch1Th.Start("Character 1");
+        ch2Th.Start("Character 2");
+        ch3Th.Start("Character 3");
+        ch1Th.Join();
+        ch2Th.Join();
+        ch3Th.Join();
+        Console.WriteLine("All characters have finished their actions.");
+    }
+
+    static void PerformAction(object characterName)
+    {
+        string name = (string)characterName;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(name+" is performing action "+i);
+            Thread.Sleep(500);
+        }
+    }
+}
