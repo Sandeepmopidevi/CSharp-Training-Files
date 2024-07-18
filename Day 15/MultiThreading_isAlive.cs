de16404e3af6a905eb6604aca264d1c90bc01167
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-18 10:25:48 +0530

    refactor: Rename parameter in Dog and Cat methods for clarity

---
+++ Day 15/MultiThreading_isAlive.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+class Animal{
+    static void Dog(int n){
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Dog is running lap"+i);
+            Console.WriteLine("Dog is Sleeping");
+        }
+        Console.WriteLine("Dog Finished Running");
+    }
+    static void Cat(int n){
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Cat is running lap"+i);
+            Console.WriteLine("Cat is Sleeping");
+        }
+        Console.WriteLine("Cat Finished Running");
+    }
+    static void Main(){
+        Thread t1 = new Thread(() => Dog(5));
+        Thread t2 = new Thread(() => Cat(3));
+        t1.Start();
+        t2.Start();
+        Console.WriteLine("Thread 1 is Alive: "+t1.IsAlive);
+        Console.WriteLine("Thread 2 is Alive: "+t2.IsAlive);
+        t1.Join();
+        t2.Join();
+        Console.WriteLine("Thread 1 is Alive: "+t1.IsAlive);
+        Console.WriteLine("Thread 2 is Alive: "+t2.IsAlive);
+    }
+}
