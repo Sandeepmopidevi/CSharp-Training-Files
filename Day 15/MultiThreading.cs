88e867ebaa9b2aad5fb186b6ba45a59d84d25713
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-18 09:44:49 +0530

    refactor: Remove unnecessary code and optimize employee list manipulation

---
+++ Day 15/MultiThreading.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+class Animal{
+    static void Dog(int n){
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Dog is running lap"+i);
+            Console.WriteLine("Dog is Sleeping");
+        }
+        Console.WriteLine("Dog Finished Running");
+    }
+    static void Cat(int n){
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Cat is running lap"+i);
+            Console.WriteLine("Cat is Sleeping");
+        }
+        Console.WriteLine("Cat Finished Running");
+    }
+    static void Main(){
+        Thread t1 = new Thread(() => Dog(5));
+        Thread t2 = new Thread(() => Cat(3));
+        t1.Start();
+        t2.Start();
+    }
+}
