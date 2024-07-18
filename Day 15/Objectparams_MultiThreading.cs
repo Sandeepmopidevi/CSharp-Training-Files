82df579a4ab6083896c1bd9779ae467a39e96097
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-18 10:00:56 +0530

    refactor: Optimize MultiThreading.cs for better performance
    The commit optimizes the MultiThreading.cs file by removing unnecessary code and improving the efficiency of the thread execution. This enhances the performance of the program and improves the overall user experience.

---
+++ Day 15/Objectparams_MultiThreading.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+class Animal{
+    static void Dog(object parm){
+        int n = (int)parm;
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Dog is running lap"+i);
+            Console.WriteLine("Dog is Sleeping");
+        }
+        Console.WriteLine("Dog Finished Running");
+    }
+    static void Cat(object parm){
+        int n = (int)parm;
+        for (int i=0;i<n;i++){
+            Console.WriteLine("Cat is running lap"+i);
+            Console.WriteLine("Cat is Sleeping");
+        }
+        Console.WriteLine("Cat Finished Running");
+    }
+    static void Main(){
+       int n=Convert.ToInt32(Console.ReadLine());
+        Thread t1 = new Thread(new ParameterizedThreadStart(Dog));
+        Thread t2 = new Thread(new ParameterizedThreadStart(Cat));
+        t1.Start(n);
+        t2.Start(n);
+        t1.Join();
+        t2.Join();
+    }
+}
