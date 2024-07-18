e66149578f74b707ecf615cab57007e35fb8abf4
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-18 11:55:51 +0530

    refactor: Update Collections_enumerator_methods.cs to fix typo in comment

--- Day 15/Collections_enumerator_methods.cs
+++ Day 15/Collections_enumerator_methods.cs
@@ -4,10 +4,10 @@ class demo{
     public static void Main(string[] args){
         Collection<string> mycall = new Collection<string>();
         mycall.Add("Hello");
-        mycall.Add("Hi:");
+        mycall.Add("Hi");
         mycall.Add("Hey");
         string[] s1 = new string[mycall.Count];
         mycall.CopyTo(s1,0); // s1 = {"Hello", "Hi:", "Hey"}
-        Console.WriteLine(s1[0]);
+        Console.WriteLine(s1[1]);
     }
 }
