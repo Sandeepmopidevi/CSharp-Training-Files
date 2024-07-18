e861d4703cf4c34df8b523dc1e5242f7e88baae7
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-18 11:29:04 +0530

    refactor: Update CSharp Notes with information about collections

---
+++ Day 15/Collections_generic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+class C1{
+    public static void Main(string[] args){
+        Collection<string> i1=new Collection<string>();
+        i1.Add("A");
+        i1.Add("B");
+        i1.Add("C");
+        Console.WriteLine("No of Elements"+i1.Count);
+        var enumerator =i1.GetEnumerator();
+        while(enumerator.MoveNext()){
+            Console.WriteLine(enumerator.Current);
+        }
+    }
+}
