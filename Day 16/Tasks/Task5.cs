8f9f5fd28ad5feb177a46920f58432ccb9292a88
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-19 15:50:31 +0530

    refactor: Add code for color palette creation and display in Task5.cs

---
+++ Day 16/Tasks/Task5.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class ColorPalette
+{
+    static void Main()
+    {
+        List<string> colors = new List<string>();
+        Console.WriteLine("Enter colors for your palette (type 'done' to finish):");
+        string input = Console.ReadLine();
+        while (input != "done")
+        {
+            colors.Add(input);
+            input = Console.ReadLine();
+        }
+        Console.WriteLine("Colors in forward order:");
+        foreach (string color in colors)
+        {
+            Console.WriteLine(color);
+        }
+        Console.WriteLine("Colors in reverse order:");
+        for (int i = colors.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(colors[i]);
+        }
+    }
+}
