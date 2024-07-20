57714acc2fc72966caaf27226bbca9a571d6430e
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-20 10:00:50 +0530

    refactor: Add delegates for method references in C1 class

---
+++ Day 17/Delegates.cs
@@ -0,0 +1,18 @@
+using System;
+class C1{
+    public delegate void add(int a,int b);
+    public delegate void sub(int a,int b);
+    public void sum(int a,int b){
+        Console.WriteLine(a+b);
+    }
+    public void minus(int a,int b){
+        Console.WriteLine(a-b);
+    }
+    public static void Main(string[] args){
+        C1 c = new C1();
+        add a = new add(c.sum);
+        sub s = new sub(c.minus);
+        a(10,20);
+        s(30,20);
+    }
+}
