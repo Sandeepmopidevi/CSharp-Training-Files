fb161771bb3dadf5a8d3078fa2606965dbb95f5d
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-20 10:56:12 +0530

    refactor: Refactor custom-delegation.cs for improved readability and consistency

--- Day 17/custom-delegation.cs
+++ Day 17/custom-delegation.cs
@@ -1,11 +1,18 @@
 using System;
-class C3{
-    public delegate int show(int a,int b,int c);
-    public static int display(int a,int b,int c){
-        return a+b*c;
+
+class C3
+{
+    public delegate int Show(int a, int b, int c);
+
+    public static int Display(int a, int b, int c)
+    {
+        return a + b * c;
     }
-    static void Main(string[] args){
-        show s= display;
-        Console.WriteLine(s(10,20,30));
+
+    static void Main(string[] args)
+    {
+        Show s = Display;
+        //s(10,20,30)
+        Console.WriteLine(s.Invoke(10, 20, 30));
     }
 }
