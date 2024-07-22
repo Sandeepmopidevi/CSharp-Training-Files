e8bfa272723db87aaa5d3d9ac918a26381efe715
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-22 15:17:26 +0530

    refactor: Remove unnecessary files and directories

--- Unit Testing/StudentGradesCalculator/StdTest.nUnitTests/GradeCal.cs
+++ Unit Testing/StudentGradesCalculator/StdTest.nUnitTests/GradeCal.cs
@@ -18,6 +18,48 @@ namespace StdTest.nUnitTests
             var grade = st.GetGradesByPercentage(percentage);
             Assert.AreEqual("A+",grade);
         }
+        [Test]
+        public void GetGradesByPercentage_Test2()
+        {
+            var percentage = 89;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("A", grade);
+        }
+        [Test]
+        public void GetGradesByPercentage_Test3()
+        {
+            var percentage = 79;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("B", grade);
+        }
+        [Test]
+        public void GetGradesByPercentage_Test4()
+        {
+            var percentage = 69;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("C", grade);
+        }
+        [Test]
+        public void GetGradesByPercentage_Test5()
+        {
+            var percentage = 59;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("D", grade);
+        }
+        [Test]
+        public void GetGradesByPercentage_Test6()
+        {
+            var percentage = 49;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("E", grade);
+        }
+        [Test]
+        public void GetGradesByPercentage_Test7()
+        {
+            var percentage = 38;
+            var grade = st.GetGradesByPercentage(percentage);
+            Assert.AreEqual("Fail", grade);
+        }
     }
 
 }
