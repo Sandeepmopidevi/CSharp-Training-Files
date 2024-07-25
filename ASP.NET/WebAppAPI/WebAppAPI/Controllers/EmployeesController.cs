db19280ed79acd86af1d37e8944b07b5fccbfe42
Sandeep Mopidevi <sandeepmopidevi8537@gmail.com>
2024-07-25 12:42:54 +0530

    added API

---
+++ ASP.NET/WebAppAPI/WebAppAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppAPI.Data;
+using WebAppAPI.Model;
+using WebAppAPI.Model.Entities;
+namespace WebAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = dbContext.Employees.ToList();
+            return Ok(employees);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployee)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployee.Name,
+                Email = addEmployee.Email,
+                Phone = addEmployee.Phone,
+                Salary = addEmployee.Salary
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+
+            
+        }
+    }
+}
